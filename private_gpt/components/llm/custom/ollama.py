bf7861d47693174839f2f1adf9b03d772dcda0fb
rboone <rboone@sofics.com>
2024-03-27 17:00:12 +0100

    final keep_alive fix hopefully

--- private_gpt/components/llm/custom/ollama.py
+++ private_gpt/components/llm/custom/ollama.py
@@ -1,10 +1,5 @@
-import logging
-from typing import Any
-
 from llama_index.llms.ollama import Ollama
 from pydantic import Field
-
-logger = logging.getLogger(__name__)
 
 
 class CustomOllama(Ollama):
@@ -17,17 +12,21 @@ class CustomOllama(Ollama):
 
     def __init__(self, *args, **kwargs) -> None:
         keep_alive = kwargs.pop('keep_alive', '5m')  # fetch keep_alive from kwargs or use 5m if not found.
-        logger.warning("########## %s ##########", keep_alive)
         super().__init__(*args, **kwargs)
         self.keep_alive = keep_alive
 
-    def _get_all_kwargs(self, **kwargs: Any) -> dict:
-        post_req_remaining_dict = {
-            **self._model_kwargs,
-            **kwargs,
-            "keep_alive": self.keep_alive
-        }
+    def chat(self, *args, **kwargs):
+        kwargs["keep_alive"] = self.keep_alive
+        return super().chat(*args, **kwargs)
 
-        logger.warning("########## %s ##########", post_req_remaining_dict)
+    def stream_chat(self, *args, **kwargs):
+        kwargs["keep_alive"] = self.keep_alive
+        return super().stream_chat(*args, **kwargs)
 
-        return post_req_remaining_dict
+    def complete(self, *args, **kwargs):
+        kwargs["keep_alive"] = self.keep_alive
+        return super().complete(*args, **kwargs)
+
+    def stream_complete(self, *args, **kwargs):
+        kwargs["keep_alive"] = self.keep_alive
+        return super().stream_complete(*args, **kwargs)

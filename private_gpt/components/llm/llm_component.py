4a7c18fad09051e16062b7020638a826a32491c9
Robin Boone <rboone@sofics.com>
2024-03-29 10:13:05 +0100

    mypy stuff

--- private_gpt/components/llm/llm_component.py
+++ private_gpt/components/llm/llm_component.py
@@ -1,5 +1,6 @@
 import logging
 from collections.abc import Callable
+from typing import Any
 
 from injector import inject, singleton
 from llama_index.core.llms import LLM, MockLLM
@@ -137,8 +138,8 @@ class LLMComponent:
 
                 if ollama_settings.keep_alive != ollama_settings.model_fields["keep_alive"].default:
                     # Modify Ollama methods to use the "keep_alive" field.
-                    def add_keep_alive(func: Callable) -> Callable:
-                        def wrapper(*args, **kwargs) -> Callable:
+                    def add_keep_alive(func: Callable[..., Any]) -> Callable[..., Any]:
+                        def wrapper(*args: Any, **kwargs: Any) -> Any:
                             kwargs["keep_alive"] = ollama_settings.keep_alive
                             return func(*args, **kwargs)
 
